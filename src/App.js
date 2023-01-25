d498850b13673294fa93fd60b693c4ea6f818bf9
Vion-sis <79034712+Vion-sis@users.noreply.github.com>
2023-01-26 05:21:03 +0900

    created a form and rendered word definitions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Form from "./Form";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">Hello</div>
+      <div className="App-header">
+        <h1>Dictionary</h1>
+        <h4>What word are you searching for?</h4>
+      </div>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results.js";
+
+import "./Form.css";
+
+export default function Form() {
+  const [word, setWord] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+
+  function showTheWordMeaning(response) {
+    setSearchResults(response.data[0]);
+    console.log(response.data[0]);
+  }
+
+  function searchWord() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(showTheWordMeaning);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchWord();
+  }
+
+  function updateWordUpdate(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div>
+      <div className="Form">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={updateWordUpdate}
+              className="me-2 btn btn-light"
+            />
+            <input type="submit" value={"search"} className="btn btn-info" />
+          </form>
+        </div>
+      </div>
+      <Results results={searchResults} />
+    </div>
+  );
+}
